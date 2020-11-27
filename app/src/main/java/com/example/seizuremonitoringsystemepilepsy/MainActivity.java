f04f909c8435aba5fa896c14a12dd22c1bead817
Arjun Vijayanatha Kurup <arjunvijayanathakurup@gmail.com>
2020-11-27 20:39:13 +0530

    Added splashscreen

--- app/src/main/java/com/example/seizuremonitoringsystemepilepsy/MainActivity.java
+++ app/src/main/java/com/example/seizuremonitoringsystemepilepsy/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.seizuremonitoringsystemepilepsy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final int SPLASH_DELAY_LENGTH = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        requestWindowFeature(Window.FEATURE_NO_TITLE);
+//        getSupportActionBar().hide(); // hide the title bar
+//        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+//                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(MainActivity.this, LoginSignup.class);
+                MainActivity.this.startActivity(mainIntent);
+                MainActivity.this.finish();
+            }
+        }, SPLASH_DELAY_LENGTH);
     }
 }
